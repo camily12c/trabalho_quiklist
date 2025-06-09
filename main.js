293e7ae3fffd0eb050b337746bc927346eaa047b
milyhidalgo03 <milyhidalgo03@gmail.com>
2025-06-09 13:16:05 +0000

    criacao do main.js e criacao da funcao para adcionar itens array items

---
+++ main.js
@@ -0,0 +1,18 @@
+const itens = []
+
+function addItem () {
+  const itemName = document.querySelector("#item").value
+
+  console.log(itemName)
+
+  const item = {
+    name: itemName,
+    checked: false
+  }
+
+  itens.push(item)
+
+  document.querySelector("#item").value = ""
+
+
+}
